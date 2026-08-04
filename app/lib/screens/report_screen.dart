ec1c3a80384012af55cc6b3660d9f742906582d7
Aryaman <aryamansodha@gmail.com>
2026-08-05 03:40:47 +0530

    Configured the image capture function and preview display after capture

--- app/lib/screens/report_screen.dart
+++ app/lib/screens/report_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ReportScreen extends StatelessWidget {
+
+class ReportScreen extends StatefulWidget {
+
   const ReportScreen({super.key});
+
+  @override
+  State<ReportScreen> createState() => _ReportScreenState();
+
+}
+
+class _ReportScreenState extends State<ReportScreen> {
+ 
+  final ImagePicker picker = ImagePicker();
+  XFile? image;
+Future<void> captureImage() async {
+  final XFile? photo = await picker.pickImage(
+    source: ImageSource.camera,
+  );
+
+  if (photo != null) {
+    setState(() {
+      image = photo;
+    });
+
+    print(photo.path);
+  }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +41,32 @@ class ReportScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Upload Image"),
-            ),
+       Container(
+         height: 250,
+          width: double.infinity,
+        decoration: BoxDecoration(
+         border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+         ),
+            child: image == null
+        ? const Center(
+          child: Text("No image selected"),
+        )
+      : ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.file(
+            File(image!.path),
+            fit: BoxFit.cover,
+          ),
+        ),
+),
 
+        const SizedBox(height: 20),
+
+ElevatedButton(
+  onPressed: captureImage,
+  child: const Text("Capture Image"),
+),
             SizedBox(height: 20),
 
             TextField(
@@ -30,10 +78,23 @@ class ReportScreen extends StatelessWidget {
 
             SizedBox(height: 20),
 
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Get Location"),
-            ),
+       Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+         const Text(
+         "Location",
+         style: TextStyle(
+        fontWeight: FontWeight.bold,
+        ),
+      ),
+
+       const SizedBox(height: 8),
+
+        const Text(
+      "📍 Waiting for image capture...",
+       ),
+      ],
+      ),
 
             SizedBox(height: 20),
 
